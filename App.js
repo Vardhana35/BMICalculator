8d562e5f1f8debc0ffbd70ae65f24ee20415600e
BOBBURI NAGA VARDHANA <126737264+Vardhana35@users.noreply.github.com>
2023-08-14 19:38:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import BMICalc from './components/BMICalc ';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <BMICalc/>
+      </div>)
+}
+
+export default App;
